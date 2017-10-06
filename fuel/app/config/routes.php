cef24952a834f25576f1aad09810ee968d965c6b
matsui yuki <matsui-yuki@social-sharing.org>
2017-10-06 12:41:32 +0900

    関数を追加

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -3,6 +3,6 @@
  *	rootパスを指定
  */
 return array(
-	'_root_'  => 'play/index',  // The default route
+	'_root_'  => 'title/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
 );
