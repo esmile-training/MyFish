3cb68469232c83ed83f603e36bda74a46d8ee3e1
matsui yuki <matsui-yuki@social-sharing.org>
2017-10-10 12:32:20 +0900

    リソースの変更とcssの追加

--- fuel/app/views/play.php
+++ fuel/app/views/play.php
@@ -9,8 +9,13 @@
 	<?= Asset::img('bg/battle_bg.png', array('alt' => 'background')) ?>
 </div>
 
-<?php foreach ($tile_position as $tile): ?>
-	<div>
-		<?= Asset::img('object/tile.png', array('alt' => 'tile')) ?>
-	</div>
-<?php endforeach; ?>
+<div id="bord">
+	<?php // タイルを敷き詰める処理 ?>
+	<?php foreach ($tile_position as $tile): ?>
+		<div>
+			<?= Asset::img('object/tile.png', array('alt' => 'tile')) ?>
+			<?php var_dump($tile) ?>
+		</div>
+	<?php endforeach; ?>
+</div>
+
