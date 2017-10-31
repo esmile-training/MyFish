c4db84ce3287434ea2aadfb3e843461e11d7c7f0
matsui yuki <matsui-yuki@social-sharing.org>
2017-10-31 17:47:33 +0900

    記述を追加

--- fuel/app/classes/controller/ajax_test.php
+++ fuel/app/classes/controller/ajax_test.php
@@ -1,8 +1,14 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Ajaxのテスト用コントローラ
  */
-
+class Ajax_test extends Controller_Base_Game
+{
+	// index
+	public function action_index()
+	{
+		// viewへ移動する
+		View_Wrap::contents('ajax_test',$this->view_data);
+	}
+}
